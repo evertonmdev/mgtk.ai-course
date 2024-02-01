8d55dda15f65096c1809af93cb4bcf7ed86b83e3
Everton Matheus <matheus.enterprise.3578@gmail.com>
2024-02-01 13:33:21 -0300

    Add dynamic export to GET function

--- src/app/(backend)/api/get-all/route.ts
+++ src/app/(backend)/api/get-all/route.ts
@@ -1,9 +1,8 @@
-"use server";
 import { getAllCourses } from "@/services/backend/get-all-courses";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
 export async function GET() {
     const all = await getAllCourses()
-
     return NextResponse.json(all)
 }
